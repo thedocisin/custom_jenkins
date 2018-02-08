a280fe9d00bee7361c74aaf7363ba95f7bc71a16
Your Name <you@example.com>
2018-02-08 14:45:27 +0000

    install composer script added

---
+++ install_composer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+COMPOSER_VERSION=${COMPOSER_VERSION:-1.6.3}
+
+curl -s -f -L -o /tmp/installer.php https://raw.githubusercontent.com/composer/getcomposer.org/da290238de6d63faace0343efbdd5aa9354332c5/web/installer \
+ && php -r " \
+    \$signature = '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061'; \
+    \$hash = hash('SHA384', file_get_contents('/tmp/installer.php')); \
+    if (!hash_equals(\$signature, \$hash)) { \
+        unlink('/tmp/installer.php'); \
+        echo 'Integrity check failed, installer is either corrupt or worse.' . PHP_EOL; \
+        exit(1); \
+    }" \
+ && php /tmp/installer.php --no-ansi --install-dir=/usr/bin --filename=composer --version=${COMPOSER_VERSION} \
+ && rm /tmp/installer.php \
+ && composer --ansi --version --no-interaction
